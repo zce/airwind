2e6f431a5fb775316014ab6189077389a4d50d38
zce <w@zce.me>
2021-01-23 15:44:27 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <img class="absolute z-0" src="./assets/background.png" alt="Background">
+  <Picker :data="temps" v-model="selected"/>
+</template>
+
+<script setup>
+// 2560x1600
+import { ref } from 'vue'
+import Picker from './components/Picker.vue'
+const temps = Array(33).fill(16).map((item, i) => item + i * 0.5)
+const selected = ref(24.5)
+</script>
---
+++ src/components/Picker.vue
@@ -0,0 +1,16 @@
+<template>
+  <select v-model="selected" multiple>
+    <option v-for="item in data" :key="item">{{ item }}</option>
+  </select>
+</template>
+
+<script setup>
+import { defineProps, reactive } from 'vue'
+
+defineProps({
+  data: Array,
+  selected: Number
+})
+
+const state = reactive({ count: 0 })
+</script>
