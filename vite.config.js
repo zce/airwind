c91f26985d14dcdbef8f12e47cf1c2448ea200f6
zce <w@zce.me>
2021-02-09 12:40:50 +0800

    feat: initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import vue from '@vitejs/plugin-vue'
-import legacy from '@vitejs/plugin-legacy'
 
 /** @type {import('vite').UserConfig} */
-export default {
-  base: '',
-  plugins: [vue(), legacy({ targets: 'IE 11' })]
+const config = {
+  // base: '',
+  build: {
+    outDir: 'www'
+  },
+  plugins: [vue()]
 }
+
+export default config
