1506f749a7c782e95dda95d138bb734ed684a348
zce <w@zce.me>
2021-03-14 18:19:25 +0800

    feat: update

--- usb2lin/src/android/USB2LIN.java
+++ usb2lin/src/android/USB2LIN.java
@@ -7,28 +7,29 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.util.Log;
+
 /**
  * This class echoes a string called from JavaScript.
  */
 public class USB2LIN extends CordovaPlugin {
 
     @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("update")) {
-            int id = args.getInt(0);
-            int vertical = args.getInt(1);
-            int horizontal = args.getInt(2);
-            this.update(id, vertical, horizontal, callbackContext);
-            return true;
-        }
-        return false;
+    public boolean execute(String action, JSONArray args, CallbackContext callback) throws JSONException {
+        if (!action.equals("update")) return false;
+        int id = args.getInt(0);
+        int vertical = args.getInt(1);
+        int horizontal = args.getInt(2);
+        this.update(id, vertical, horizontal, callback);
+        return true;
     }
 
-    private void update(int id, int vertical, int horizontal, CallbackContext callbackContext) {
+    private void update(int id, int vertical, int horizontal, CallbackContext callback) {
         // TODO: to lin
+        Log.i("USB2LIN", "id: " + id + ", vertical:" + vertical + ", horizontal:" + horizontal);
         // 成功回調
-        callbackContext.success(id);
+        callback.success(id);
         // 失敗回調
-        // callbackContext.error("message");
+        // callback.error("message");
     }
 }
