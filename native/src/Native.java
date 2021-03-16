1e3c32da338c056478b0dc97b3853833994caac2
zce <w@zce.me>
2021-03-16 13:22:08 +0800

    feat: initial commit

--- native/src/Native.java
+++ native/src/Native.java
@@ -15,24 +15,33 @@ import android.util.Log;
  * This class echoes a string called from JavaScript.
  */
 public class Native extends CordovaPlugin {
+    private static final String TAG = "USB2LIN";
+
+    private USBManager usbManager = new USBManager();
+
     private int[] devices = new int[10];
     private int deviceCount = 0;
 
     private void update(int id, int vertical, int horizontal, CallbackContext callback) {
         // // TODO: to lin
-        // Log.i("USB2LIN", "id: " + id + ", vertical:" + vertical + ", horizontal:" + horizontal);
+        // Log.i(TAG, "id: " + id + ", vertical:" + vertical + ", horizontal:" + horizontal);
+        // 没找到设备
+        if (devices[id] == 0) {
+            Log.i(TAG, "没找到 " + id + " 设备");
+            return;
+        }
 
         // 这里发送数据
 
         // 主机模式发送数据
         byte write_pid = 0x11;
         byte[] write_data = {0x01, 0x00, 0x00, (byte) 0xd0, 0x00, 0x00, 0x00, 0x00};
-        int ret = USB2LINEX.INSTANCE.LIN_EX_MasterWrite(id, (byte) 0, write_pid, write_data, (byte) write_data.length, USB2LINEX.LIN_EX_CHECK_EXT);
+        int ret = USB2LINEX.INSTANCE.LIN_EX_MasterWrite(devices[id], (byte) 0, write_pid, write_data, (byte) write_data.length, USB2LINEX.LIN_EX_CHECK_EXT);
 
         if (ret == USB2LINEX.LIN_EX_SUCCESS) {
-            Log.i("USB2LIN", "【" + id + "】信号发送成功");
+            Log.i(TAG, "【" + id + "】信号发送成功");
         } else {
-            Log.i("USB2LIN", "【" + id + "】信号发送失败");
+            Log.i(TAG, "【" + id + "】信号发送失败");
         }
 
         // 成功回调
@@ -43,18 +52,25 @@ public class Native extends CordovaPlugin {
 
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
-        Log.i("USB2LIN", "开始初始化设备");
+        Log.i(TAG, "开始初始化 USB");
+
+        if (!usbManager.USBInit(cordova.getActivity().getApplicationContext())) {
+            Log.i(TAG, "初始化 USB 失败");
+            return;
+        }
+
+        Log.i(TAG, "开始初始化设备");
 
         // 扫描设备
         deviceCount = USBDevice.INSTANCE.USB_ScanDevice(devices);
 
         // 没找到设备
         if (deviceCount == 0) {
-            Log.i("USB2LIN", "没找到设备");
+            Log.i(TAG, "没找到设备");
             return;
         }
 
-        Log.i("USB2LIN", "发现 " + deviceCount + " 个设备");
+        Log.i(TAG, "发现 " + deviceCount + " 个设备");
 
         // 打开全部设备
         for (int i = 0; i < deviceCount; i++) {
@@ -63,11 +79,11 @@ public class Native extends CordovaPlugin {
             boolean state = USBDevice.INSTANCE.USB_OpenDevice(device);
 
             if (!state) {
-                Log.i("USB2LIN", "打开设备【" + device + "】失败");
+                Log.i(TAG, "打开设备【" + device + "】失败");
                 return;
             }
 
-            Log.i("USB2LIN", "打开设备【" + device + "】成功");
+            Log.i(TAG, "打开设备【" + device + "】成功");
 
             try {
                 USBDevice.DEVICE_INFO info = new USBDevice.DEVICE_INFO();
@@ -76,15 +92,15 @@ public class Native extends CordovaPlugin {
                 state = USBDevice.INSTANCE.DEV_GetDeviceInfo(device, info, introduce);
                 if (!state) {
                     USBDevice.INSTANCE.USB_CloseDevice(device);
-                    Log.i("USB2LIN", "获取设备【" + device + "】信息失败");
+                    Log.i(TAG, "获取设备【" + device + "】信息失败");
                     return;
                 }
 
-                Log.i("USB2LIN", "【" + device + "】Name: " + info.FirmwareName);
-                Log.i("USB2LIN", "【" + device + "】Build Date: " + info.BuildDate);
-                Log.i("USB2LIN", "【" + device + "】" + String.format("Firmware Version: v%d.%d.%d", (info.FirmwareVersion >> 24) & 0xFF, (info.FirmwareVersion >> 16) & 0xFF, info.FirmwareVersion & 0xFFFF));
-                Log.i("USB2LIN", "【" + device + "】" + String.format("Hardware Version: v%d.%d.%d", (info.HardwareVersion >> 24) & 0xFF, (info.HardwareVersion >> 16) & 0xFF, info.HardwareVersion & 0xFFFF));
-                Log.i("USB2LIN", "【" + device + "】Functions: " + introduce);
+                Log.i(TAG, "【" + device + "】Name: " + info.FirmwareName);
+                Log.i(TAG, "【" + device + "】Build Date: " + info.BuildDate);
+                Log.i(TAG, "【" + device + "】" + String.format("Firmware Version: v%d.%d.%d", (info.FirmwareVersion >> 24) & 0xFF, (info.FirmwareVersion >> 16) & 0xFF, info.FirmwareVersion & 0xFFFF));
+                Log.i(TAG, "【" + device + "】" + String.format("Hardware Version: v%d.%d.%d", (info.HardwareVersion >> 24) & 0xFF, (info.HardwareVersion >> 16) & 0xFF, info.HardwareVersion & 0xFFFF));
+                Log.i(TAG, "【" + device + "】Functions: " + introduce);
             } catch (Exception ep) {
                 ep.printStackTrace();
             }
@@ -92,9 +108,9 @@ public class Native extends CordovaPlugin {
             // 初始化设备
             int result = USB2LINEX.INSTANCE.LIN_EX_Init(device, (byte) 0, 19200, (byte) 1);
             if (result == USB2LINEX.LIN_EX_SUCCESS) {
-                Log.i("USB2LIN", "初始化设备【" + device + "】成功");
+                Log.i(TAG, "初始化设备【" + device + "】成功");
             } else {
-                Log.i("USB2LIN", "初始化设备【" + device + "】失败: " + result);
+                Log.i(TAG, "初始化设备【" + device + "】失败: " + result);
             }
         }
     }
@@ -107,5 +123,20 @@ public class Native extends CordovaPlugin {
         int horizontal = args.getInt(2);
         this.update(id, vertical, horizontal, callback);
         return true;
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i(TAG, "尝试关闭所有设备");
+        // 关闭每一个设备
+        for (int i = 0; i < deviceCount; i++) {
+            int device = devices[i];
+            boolean succeed = USBDevice.INSTANCE.USB_CloseDevice(device);
+            if (succeed) {
+                Log.i(TAG, "关闭设备【" + device + "】成功");
+            } else {
+                Log.i(TAG, "关闭设备【" + device + "】失败");
+            }
+        }
     }
 }
